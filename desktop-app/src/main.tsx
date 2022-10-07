28b3aa82a11c5247c14ec45bf98f145cf5a535f2
hoanghy <hoanghy01122003@gmail.com>
2022-10-07 23:37:23 +0700

    Initial project

---
+++ desktop-app/src/main.tsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// // import App from './App'
+// import App from '../../src/App'
+// import 'styles/index.css'
+
+// /**
+//  * If you enables use of Node.js API in the Renderer-process
+//  * ```
+//  * npm i -D vite-plugin-electron-renderer
+//  * ```
+//  * @see - https://github.com/electron-vite/vite-plugin-electron/tree/main/packages/electron-renderer#electron-renderervite-serve
+//  */
+// // import './samples/node-api'
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+import '../../src/main'
+
+postMessage({ payload: 'removeLoading' }, '*')
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+// import "./App.scss";
+import styles from "./App.module.scss";
+
+function App() {
+  return <div className={styles.App}>Source code</div>;
+}
+
+export default App;
