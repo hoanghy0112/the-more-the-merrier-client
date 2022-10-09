52be466febb243b364675aa3966f3dea461df5c2
hoanghy <hoanghy01122003@gmail.com>
2022-10-09 23:27:42 +0700

    update vite

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -2,9 +2,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import dns from 'dns';
+
+dns.setDefaultResultOrder('verbatim');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port: 8000,
+  },
   build: {
     outDir: '../build/web',
   },
