6f642c910deb9eebefe178711aa0de137ebe4a39
Quang Duong <quangduongptsc@gmail.com>
2022-10-13 14:20:48 +0700

    fix tag detail

--- src/components/TagDetail/TagDetail.jsx
+++ src/components/TagDetail/TagDetail.jsx
@@ -12,7 +12,7 @@ const color = [
 
 const TagDetail = ({ tagTitle, description, listTasks }) => {
 
-    const [viewTasks, setViewTasks] = useState(listTasks.slice(0, 2))
+    const [viewTasks, setViewTasks] = useState(listTasks.length > 2 ? listTasks.slice(0, 2) : listTasks)
 
     const [isEdit, setIsEdit] = useState(false)
 
@@ -21,7 +21,7 @@ const TagDetail = ({ tagTitle, description, listTasks }) => {
     const [newDescription, setNewDescription] = useState(description)
 
     const handleChangeView = () => {
-        if(viewTasks.length > 2) 
+        if (viewTasks.length > 2)
             setViewTasks(listTasks.slice(0, 2))
         else
             setViewTasks(listTasks)
@@ -31,7 +31,7 @@ const TagDetail = ({ tagTitle, description, listTasks }) => {
         <div className="tag-detail-container">
             <div className="tag-detail-header">
                 <div className="tag-name">
-                    {isEdit===false ? tagTitle : <input value={newTitle} onChange={(e) => setNewTitle(e.target.value)}></input>}
+                    {isEdit === false ? tagTitle : <input value={newTitle} onChange={(e) => setNewTitle(e.target.value)}></input>}
                     <div className="tag-icon">
                         <img src={ICON_FOLDER}></img>
                     </div>
@@ -41,27 +41,30 @@ const TagDetail = ({ tagTitle, description, listTasks }) => {
                 </div>
             </div>
             <div className="tag-detail-description">
-                {isEdit===false ? description : <textarea value={newDescription} onChange={(e) => setNewDescription(e.target.value)}></textarea>}
+                {isEdit === false ? description : <textarea value={newDescription} onChange={(e) => setNewDescription(e.target.value)}></textarea>}
             </div>
             <div className="tag-detail-tasks">
                 {viewTasks && viewTasks.length > 0 && viewTasks.map(task => {
                     return (
-                        <div key={task.id} className="task" style={{ background: color[task.priority - 1]}}>
+                        <div key={task.id} className="task" style={{ background: color[task.priority - 1] }}>
                             <div className="task-item">{task.text}</div>
                             <div className="task-item">
                                 {task.start} - {task.end}
-                                {isEdit && <ICON_DEL style={{marginLeft: '15px', cursor: 'pointer'}} />}
+                                {isEdit && <ICON_DEL style={{ marginLeft: '15px', cursor: 'pointer' }} />}
                             </div>
                         </div>
                     )
                 })}
             </div>
-            <div className="full-tasks" onClick={() => handleChangeView()}>
-                <div className="tag-icon">
-                    <img src={ICON_FULL_ARROW_DOWN}></img>
+            {
+                listTasks.length > 2 &&
+                <div className="full-tasks" onClick={() => handleChangeView()}>
+                    <div className="tag-icon">
+                        <img src={ICON_FULL_ARROW_DOWN}></img>
+                    </div>
+                    <div className="full-tasks-btn">{viewTasks.length == 2 ? "More tasks" : "Fewer Tasks"}</div>
                 </div>
-                <div className="full-tasks-btn">{viewTasks.length == 2 ? "More tasks" : "Fewer Tasks"}</div>
-            </div>
+            }
         </div>
     )
 }
@@ -73,7 +76,7 @@ TagDetail.propTypes = {
     description: PropTypes.string.isRequired,
     listTasks: PropTypes.arrayOf(
         PropTypes.shape({
-            id : PropTypes.number.isRequired,
+            id: PropTypes.number.isRequired,
             text: PropTypes.string.isRequired,
             start: PropTypes.string.isRequired,
             end: PropTypes.string.isRequired,
--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-// import Calendar from '../../components/QDComponents/Calendar/Calendar';
+// import TagDetail from '../../components/TagDetail/TagDetail'
+
+// const listTasks = [
+//   {
+//     id: 1,
+//     text: 'event 1',
+//     start: '14h',
+//     end: '18h',
+//     priority: 1
+//   },
+//   {
+//     id: 1,
+//     text: 'event 1',
+//     start: '14h',
+//     end: '18h',
+//     priority: 1
+//   },
+// ]
 
 export default function TestPage() {
-  return <div />;
+  return (
+    // <TagDetail tagTitle={"uit"} description="abc" listTasks={listTasks} />
+    <div></div>
+  );
 }
