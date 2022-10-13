eea539dd145f84e4bfa576ea137d3065db293266
hoanghy <hoanghy01122003@gmail.com>
2022-10-13 13:32:40 +0700

    update getUserProfile API

---
+++ src/constants/API_LINK.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://hoanghy.tech';
+
+export const GET_USER_PROFILE_API_LINK = `${BASE_URL}/api/v1/user`;
--- src/features/userManagement/profileAPI.js
+++ src/features/userManagement/profileAPI.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { GET_USER_PROFILE_API_LINK } from '../../constants/API_LINK';
 import { auth } from '../../firebase/signInWithGoogleAPI';
 
 export async function getUserProfileAPI() {
   const accessToken = await auth.currentUser.getIdToken();
-  const data = await axios.get('https://localhost/api/v1/user', {
+  const data = await axios.get(GET_USER_PROFILE_API_LINK, {
     withCredentials: true,
     headers: {
       Authorization: accessToken,
