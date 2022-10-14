5b5247955ae0f9a71a1cd7c39088a7e5fdbad437
NTKhang <21520963@gm.uit.edu.vn>
2022-10-14 12:18:39 +0700

    update descriptionPopUp editting function

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,22 +5,12 @@ import styles from './HomePage.module.scss';
 
 export default function HomePage() {
   return (
-<<<<<<< HEAD
-    // <Calendar />
-    <div>
-      This is Home page
-      <TabButton isSelected={false} />
-      <Tag type="tagPending" shape="rectangle" input="UIT" />
-      <DateTimePicker startDay={new Date()} hanldeChangeStartDay={() => {}} />
-      <Calendar />
-=======
     <div className={styles.container}>
       <div className={styles.informationBox}>InformationBox</div>
       <div className={styles.mainBox}>
         <div className={styles.header}>Logo</div>
         <Outlet />
       </div>
->>>>>>> af46f53adf33c581bedd4b8aa1eb8e2c3a176281
     </div>
   );
 }
