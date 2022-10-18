572906f657c6a22263f33faac5f37b81a0e2559b
hoanghy <hoanghy01122003@gmail.com>
2022-10-18 22:50:02 +0700

    update

--- src/firebase/signInWithGoogleAPI.js
+++ src/firebase/signInWithGoogleAPI.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 import app from './configuration';
 
 const provider = new GoogleAuthProvider();
@@ -7,7 +7,7 @@ export const auth = getAuth(app);
 
 export default async function signInWithGoogleAPI() {
   return new Promise((resolve) => {
-    signInWithPopup(auth, provider).then((result) => {
+    signInWithRedirect(auth, provider).then((result) => {
       const { user } = result;
 
       resolve(user);
