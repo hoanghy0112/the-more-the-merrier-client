e262263993069b5a00f619775d1ac42a624f7ee7
hoanghy <hoanghy01122003@gmail.com>
2022-10-26 01:56:06 +0700

    remove testPage2 from router

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -34,11 +34,11 @@ const router = createBrowserRouter([
       },
     ],
   },
-  {
-    path: 'test2',
-    element: <TestPage2 />,
-    errorElement: <ErrorPage />,
-  },
+  // {
+  //   path: 'test2',
+  //   element: <TestPage2 />,
+  //   errorElement: <ErrorPage />,
+  // },
   {
     path: 'authentication',
     element: <LoginPage />,
