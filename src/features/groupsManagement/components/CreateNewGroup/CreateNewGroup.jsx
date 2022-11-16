eee2d6c51d90dca0992219e71a089f1b5ffa4ed6
NTKhang <21520963@gm.uit.edu.vn>
2022-11-16 23:56:57 +0700

    update call api create new group

--- src/features/groupsManagement/components/CreateNewGroup/CreateNewGroup.jsx
+++ src/features/groupsManagement/components/CreateNewGroup/CreateNewGroup.jsx
@@ -1,11 +1,25 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { ICON_ADD, ICON_FULL_ARROW_RIGHT } from '../../../../assets/icons';
+import {
+  getUserProfile,
+  selectUserProfile,
+} from '../../../userManagement/ProfileSlice';
+import { createNewGroup, selectAllGroups } from '../../groupSlice';
 
 import styles from './CreateNewGroup.module.scss';
 
 export default function CreateNewGroup() {
   const titleRef = useRef();
   const [description, setDescription] = useState('');
+  const dispatch = useDispatch();
+  const user = useSelector(selectUserProfile);
+  // console.log('user: ', user._id);
+  const users = [];
+
+  const handleCreateCLick = () => {
+    dispatch(createNewGroup(user.engName, description, users, user._id));
+  };
 
   return (
     <div className={styles.container}>
@@ -34,7 +48,11 @@ export default function CreateNewGroup() {
           Add user
         </button>
       </div>
-      <button type="button" className={styles.createButton}>
+      <button
+        type="button"
+        className={styles.createButton}
+        onClick={handleCreateCLick}
+      >
         Create
         <span>
           <img src={ICON_FULL_ARROW_RIGHT} alt="" />
