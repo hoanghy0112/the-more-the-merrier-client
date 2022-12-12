8f24475c5aa75b73b85c0ff674329a174d14ce7d
Nguyen Quoc Khanh <21520982@ms.uit.edu.vn>
2022-12-12 23:47:18 +0700

    complete busyChart

--- src/components/UserProfile/BusyChart/BusyChart.jsx
+++ src/components/UserProfile/BusyChart/BusyChart.jsx
@@ -1,15 +1,63 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-
+import React, { useEffect, useState } from 'react';
+import { selectCurrentWeekTasks } from '../../../features/tasksManagement/TasksSlice';
+import { useSelector } from 'react-redux';
 import styles from './BusyChart.module.scss';
+import { UserData } from './BusyChartData';
 
+import { Bar, Line } from "react-chartjs-2";
+
+const now = new Date();
+const date = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() - now.getDay() + 1,
+);
+
+const labels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 export default function BusyChart() {
-    return (
-        <div>
-            <p className={styles.header}><span className={styles.highlighted}>Busy</span> chart</p>
-            <div className={styles.chartContainer}>
-                Busy chart
-            </div>
-        </div>
-    );
+  const now = new Date();
+  const [date, setDate] = useState(
+    new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate() - now.getDay() + 1,
+    ),
+  );
+  const tasks = useSelector(selectCurrentWeekTasks(date));
+
+  function getWorkHour(dayOfWeek) {
+    var count = 0;
+    for (let i=0; i<tasks.length; i++)
+    {
+      var taskDate = new Date(tasks[i].time.from);
+      var taskDayOfWeek = labels[taskDate.getDay()];
+      if (taskDayOfWeek === dayOfWeek)
+      {
+        count += ((new Date(tasks[i].time.to).getTime() - new Date(tasks[i].time.from).getTime()) / 86400000) * 24;
+      }
+    }
+    return count;
+  }
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Hours',
+        data: labels.map((label) => getWorkHour(label)),
+        // data: [4, 0, 2, 1, 3, 4, 5],
+        backgroundColor: '#00A6CA', 
+        borderColor: "#00A6CA",
+        borderWidth: 2,
+      },
+    ],
+  }
+  return (
+    <div>
+      <p className={styles.header}><span className={styles.highlighted}>Busy</span> chart</p>
+      <div className={styles.chartContainer}>
+        <Line data={data} width={"600px"} />
+      </div>
+    </div>
+  );
 }
