1bb34e91728596737daa566238547824ec776d6a
Hoang Hy <hoanghy01122003@gmail.com>
2023-02-08 04:31:35 +0700

    update

--- src/components/TimeTag/TimeTag.jsx
+++ src/components/TimeTag/TimeTag.jsx
@@ -9,25 +9,23 @@ import useComponentVisible from '../../hooks/useComponentVisible';
 
 export default function TimeTag({ time, onChange, disabled = false }) {
   // const [value, setValue] = useState(new Date(time));
-  const [hour, setHour] = useState(
-    String(new Date(time).getHours()).padStart(2, '0'),
-  );
-  const [minute, setMinute] = useState(
-    String(new Date(time).getMinutes()).padStart(2, '0'),
-  );
+  const [hour, setHour] = useState(new Date(time).getHours());
+  const [minute, setMinute] = useState(new Date(time).getMinutes());
   const [period, setPeriod] = useState(
     new Date(time).getHours() < 12 ? 'AM' : 'PM',
   );
 
   const [isEdit, setIsEdit] = useState(false);
-  const { ref, refCallback, isComponentVisible, setIsComponentVisible } =
+  const { ref, isComponentVisible, setIsComponentVisible } =
     useComponentVisible(false);
 
   function handleChangeHour(e) {
     const hourString = e.target.value;
     const hourNumber = parseInt(hourString, 10);
-    if (hourNumber < 12) {
-      setHour(String(hourNumber).padStart(2, '0'));
+    if (hourNumber < 24) {
+      setHour(hourNumber);
+      if (hourNumber >= 12) setPeriod('PM');
+      else setPeriod('AM');
     }
   }
 
@@ -35,15 +33,24 @@ export default function TimeTag({ time, onChange, disabled = false }) {
     const minuteString = e.target.value;
     const minuteNumber = parseInt(minuteString, 10);
     if (minuteNumber < 60) {
-      setMinute(String(minuteNumber).padStart(2, '0'));
+      setMinute(minuteNumber);
+    }
+  }
+
+  function handleChangePeriod(newPeriod) {
+    setPeriod(newPeriod);
+    if (newPeriod === 'AM' && hour >= 12) {
+      setHour(hour - 12);
+    } else if (newPeriod === 'PM' && hour < 12) {
+      setHour(hour + 12);
     }
   }
 
   useEffect(() => {
     const newDate = new Date(time);
-    newDate.setHours(parseInt(hour, 10) + (period === 'AM' ? 0 : 12));
+    newDate.setHours(parseInt(hour, 10));
     newDate.setMinutes(parseInt(minute, 10));
-    onChange(newDate);
+    // onChange(newDate);
   }, [hour, minute, period]);
 
   useEffect(() => {
@@ -63,27 +70,29 @@ export default function TimeTag({ time, onChange, disabled = false }) {
         <div className={styles.hourMinute}>
           <input
             type="text"
-            value={hour}
+            value={String(hour).padStart(2, '0')}
             onChange={handleChangeHour}
             onFocus={(e) => e.target.select()}
+            disabled={disabled}
           />
           <p>h</p>
           <input
             type="text"
-            value={minute}
+            value={String(minute).padStart(2, '0')}
             onChange={handleChangeMinute}
             onFocus={(e) => e.target.select()}
+            disabled={disabled}
           />
         </div>
         <div className={styles.apm}>
           {period === 'AM' ? (
             <>
               <p className={styles.choose}>AM</p>
-              <p onClick={() => setPeriod('PM')}>PM</p>
+              <p onClick={() => disabled || handleChangePeriod('PM')}>PM</p>
             </>
           ) : (
             <>
-              <p onClick={() => setPeriod('AM')}>AM</p>
+              <p onClick={() => disabled || handleChangePeriod('AM')}>AM</p>
               <p className={styles.choose}>PM</p>
             </>
           )}
